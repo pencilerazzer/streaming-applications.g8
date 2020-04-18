fac707ea6856dc5bcb45b25794cebca7979f4ffa
dhillonr <rohitdhillon737@gmail.com>
2020-04-19 01:08:12 +0530

    modified codegen

--- src/main/g8/codegen/src/main/scala/mycodegen/Main.scala
+++ src/main/g8/codegen/src/main/scala/mycodegen/Main.scala
@@ -15,28 +15,28 @@ object Main {
 
   import utils.ConfigJsonSupport.MyJsonProtocol._
 
-  val fileContents = Source.fromFile("codegen/src/main/resources/streamlets_info.json").getLines.mkString
-  val streamletsInfoSupports = JsonParser(fileContents).convertTo[StreamletsInfoSupport](streamletsInfoSupport)
-
   def main(args: Array[String]): Unit = {
-    args.toList match {
-      case path :: Nil if path.endsWith(".scala") =>
-        val jfile = new File(path)
-        jfile.getParentFile.mkdirs()
-        // Do scala.meta code generation here.
-        val str = streamletsInfoSupports.streamlets.foldLeft(
-          """
-            |package applications
-            |import applications._
-            |import cloudflow.spark.sql.SQLImplicits._
-            |""".stripMargin)((total, next) => {
-          total + source"""${baseClassString(next.name, next.absClass, next.ports).parse[Stat].get}""".syntax + "\n"
-        })
-        Files.write(
-          jfile.toPath,
-          str.getBytes("UTF-8")
-        )
-    }
+//    val fileContents = Source.fromFile("codegen/src/main/resources/streamlets_info.json").getLines.mkString
+//    println(fileContents )
+//    val streamletsInfoSupports = JsonParser(fileContents).convertTo[StreamletsInfoSupport](streamletsInfoSupport)
+//    args.toList match {
+//      case path :: Nil if path.endsWith(".scala") =>
+//        val jfile = new File(path)
+//        jfile.getParentFile.mkdirs()
+//        // Do scala.meta code generation here.
+//        val str = streamletsInfoSupports.streamlets.foldLeft(
+//          """
+//            |package applications
+//            |import applications._
+//            |import cloudflow.spark.sql.SQLImplicits._
+//            |""".stripMargin)((total, next) => {
+//          total + source"""${baseClassString(next.name, next.absClass, next.ports).parse[Stat].get}""".syntax + "\n"
+//        })
+//        Files.write(
+//          jfile.toPath,
+//          str.getBytes("UTF-8")
+//        )
+//    }
   }
 
   def baseClassString(name: String, absClass: String, ports: List[String]) = {
